edf0b0980152f1b89130e3b4d9925583a1561a47
Riya <riyapatel.beit@yahoo.com>
2021-03-17 14:39:32 -0400

    unit testing

---
+++ test.js
@@ -0,0 +1,41 @@
+const fetch = require("node-fetch")
+let chai = require('chai')
+let chaiHttp = require('chai-http');
+const { response } = require("express");
+let expect = chai.expect
+
+chai.use(chaiHttp);
+
+describe("Testcase for VoiceSearch API", function () {
+
+    it("should return status on 200 For", function (done) {
+        chai
+            .request('http://localhost:3000')
+            .get("/")
+            .then(function (res) {
+                expect(res).to.have.status(200);
+                done();
+            })
+            .catch(function (err) {
+                // throw(err);
+                done(err);
+            });
+    });
+
+    it("should return 404 error for test(get) ", function (done) {
+        chai
+            .request('http://localhost:3000')
+            .get("/test")//i do not have emoloyee , i have employees
+            .then(function (res) {
+                expect(res).to.have.status(404);
+                done();
+            })
+            .catch(function (err) {
+                // throw(err);
+                done(err);
+            });
+    });
+
+})
+
+module.exports = response;
