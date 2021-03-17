2e0b76a5baf064dc6a7da21afd0a8780b5c6dc7f
Riya <riyapatel.beit@yahoo.com>
2021-03-17 11:47:30 -0400

    .

--- speechRecognition/index.js
+++ speechRecognition/index.js
@@ -1,6 +1,10 @@
+const http = require("http")
 const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Speech Recognition'))
-app.listen(port, () => console.log(`this application listening on port 8080`))
+app.get('/', (req, res) => {
+    res.send('<h1>Speech Recognition</h1>')}
+    )
+app.listen(port, () => {
+    console.log(`server started at port 8080`)})
