432702a4eaa757bf41e10ba0d90745e0970bddde
Marvin <mvt5393@yahoo.com>
2021-03-17 13:22:22 -0400

    Working HTML Speech Recognition that runs using NodeJS

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+app.use(express.static(__dirname + 'public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/searchBar.html'));
+})
+
+app.get('/stylesheet.css', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/stylesheet.css'))
+})
+
+app.get('/speechrecognition.js', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/speechRecognition.js'))
+})
+
+app.get('/microphone.png', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/microphone.png'))
+})
+
+app.listen(process.env.PORT || port, () => console.log(`App listening on port ${port}!`));
---
+++ public/speechRecognition.js
@@ -0,0 +1,31 @@
+function startDictation() {
+  if(window.hasOwnProperty('webkitSpeechRecognition')) {
+    var recognition = new webkitSpeechRecognition();
+
+    // Listen for single word or a phrase
+    recognition.continuous = false;
+    // Set false means that the interim results are finals and not changeable
+    recognition.interimResults = false;
+
+    // Language: 'en-US = English US' & 'en-IN = English India to handle Accent'
+    recognition.lang = "en-US";
+    recognition.lang = "en-IN";
+    // Starting Speech Recognition
+    recognition.start();
+
+    // An event handler that will run when the speech recognition service returns a result.
+    recognition.onresult = function(e) {
+      // The first [0] returns the speechrecognitionresult at postion 0 and the second [0] returns the
+      // speechrecognitionalternative at position 0. Returns a string containing the transcript of the
+      // recognized word.
+      document.getElementById('transcript').value = e.results[0][0].transcript;
+      // this stop the speechrecognition service from listening to incoming audio.
+      recognition.stop();
+      // This submit method submits the form to micSearch in HTML
+      document.getElementById('micSearch').submit();
+    };
+    recognition.onerror = function(e) {
+      recognition.stop();
+    }
+  }
+}
