a759e7e864ec3ba5e12fc9b88e2697bb8f7ba6f9
Marvin <mvt5393@yahoo.com>
2021-03-19 14:36:30 -0400

    Updated

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Import the express module by specifying the name of the module as a string ('express'), and then calling the returned object to create an Express application.
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -5,9 +6,9 @@ const port = 3000;
 // Serving files from 'public' directory.
 app.use(express.static(__dirname + 'public'));
 
-// These are the routes defined for GET method
+// These are the routes defined for GET method.
 app.get('/', (req, res) => {
-    // Transfers the file at given path
+    // Transfers the file at given path.
     res.sendFile(path.join(__dirname + '/public/searchBar.html'));
 })
 
